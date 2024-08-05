edb0320691b3c644c497bc1e9f6de2f40cc67078
nesgnas (The Sang) <sangbdt102@gmail.com>
2024-08-05 09:17:09 +0700

    temp

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const server = http.createServer((req, res) => {
     dataRouter(req, res);
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT =  3000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
